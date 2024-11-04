67815be39573e9705a82e9d2ed3d3d2ac2a5dcaf
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-11-04 09:07:55 +0100

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,11 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='NetManage')
+    subparsers = parser.add_subparsers(dest='command')
+
+    # Read
+
+
+if __name__ == '__main__':
+    main()
