ef25ab94b3bbd06f4284fa6fbd29a3fa65a86983
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-11-08 12:12:14 +0100

    Version changed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="NetManage",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
