c22fafe098ec1bb41ad7c684bac256c8384f6276
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-11-28 15:04:33 +0100

    Writing .nmcomm file

--- NetManage/utils/NMCOMM_file.py
+++ NetManage/utils/NMCOMM_file.py
@@ -44,14 +44,14 @@ def read_NMCOMM(file:str) -> COMMAND:
                 curr_section.append(new_input)
 
 
-    if not validate_NMCOM_file(meta_section, commands_section, devices_section, inputs_section):
+    if not validate_NMCOMM_file(meta_section, commands_section, devices_section, inputs_section):
         raise Exception("Invalid NMCOM file")
 
     command = COMMAND(meta_section, commands_section, devices_section, inputs_section)
     print(inputs_section[0])
     return command
 
-def validate_NMCOM_file(meta_section, commands_section, devices_section, inputs_section):
+def validate_NMCOMM_file(meta_section, commands_section, devices_section, inputs_section):
     meta_keys = meta_section.keys()
 
     if not (len(meta_keys) == 2 and "NAME" in meta_keys and "TYPE" in meta_keys):
@@ -62,7 +62,42 @@ def validate_NMCOM_file(meta_section, commands_section, devices_section, inputs_
         return False
     return True
 
-
+def write_NMCOMM(output:str, command_obj: COMMAND):
+    with open(output, 'w+', encoding="UTF-8") as f:
+        f.writelines([
+            "-- META\n",
+            f"NAME: {command_obj.NAME}\n",
+            f"TYPE: {command_obj.TYPE}\n",
+        ])
+        f.write("-- COMMANDS\n")
+        f.writelines([f"{cmd}\n" for cmd in command_obj.commands])
+        f.write("-- DEVICES\n")
+        f.writelines([f"{device}\n" for device in command_obj.devices])
+        f.write("-- INPUTS\n")
+        f.writelines([f"{inpt}\n" for inpt in command_obj.inputs_section])
 
 if __name__ == '__main__':
-    read_NMCOMM("../commands/set_hostname.nmcomm")
+
+    input1 = INPUT()
+    input1.read("TEXT;hostname;Nazwa hosta:")
+    input2 = INPUT()
+    input2.read("SUBNET;subnet;Maska:")
+
+    command = COMMAND(
+        meta_section={
+            "TYPE": "CONFIG",
+            "NAME": "TEST"
+        },
+        commands_section=[
+            "hostname 1",
+            "exit 2"
+        ],
+        devices_section=[
+            "cisco_ios",
+            "cisco_xe",
+            "cisco_asa"
+        ],
+        inputs_section=[input1, input2]
+    )
+    #write_NMCOMM("../commands/test2.nmcomm", command)
+    read_NMCOMM("../commands/komenda1.nmcomm")
